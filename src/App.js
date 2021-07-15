1516c8e36a7e554489fbe0aa9001efd71cd25fb8
Derek Louis <thedereklouis@gmail.com>
2021-07-15 19:02:12 -0400

    completed outline for middle section

--- src/App.js
+++ src/App.js
@@ -66,6 +66,31 @@ function App() {
           filtersShape={filtersShape}
           setFiltersShape={setFiltersShape}
         />
+        <div id="midBar">
+          <p id="arpeggiatorL" className="boldLabel">
+            ARPEGGIATOR
+          </p>
+          <div id="midLVBB1" className="VBarBreak" />
+          <div id="midLBB1" className="barBreak" />
+          <p id="portaL" className="boldLabel">
+            PORTA
+          </p>
+          <div id="midLVBB2" className="VBarBreak" />
+          <div id="midLBB2" className="barBreak" />
+          <p id="lfosL" className="boldLabel">
+            LFOS
+          </p>
+          <div id="midLVBB3" className="VBarBreak" />
+          <div id="midLBB3" className="barBreak" />
+          <p id="envelopesL" className="boldLabel">
+            ENVELOPES
+          </p>
+          <div id="midLVBB4" className="VBarBreak" />
+          <div id="midLBB4" className="barBreak" />
+          <p id="effectsL" className="boldLabel">
+            EFFECTS
+          </p>
+        </div>
         {/* <div className="testH" />
         <div className="testV" /> */}
       </div>
