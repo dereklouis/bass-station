28fc371a62cfd89147e1903bb5fb62f63d9ec9de
Derek Louis <thedereklouis@gmail.com>
2021-07-17 12:30:20 -0400

    completed keys topper

--- src/keysTopper.js
+++ src/keysTopper.js
@@ -49,6 +49,111 @@ const KeysTopper = () => {
       <p id="kTGlobalL" className="boldLabel2">
         Global
       </p>
+      <p id="kTLL1" className="subLabelXSmall">
+        Filter Freq
+      </p>
+      <p id="kTLL2" className="subLabelXSmall">
+        Osc Pitch
+      </p>
+      <p id="kTML1" className="subLabelXSmall">
+        LFO 1 to
+      </p>
+      <p id="kTLL3" className="subLabelXSmall">
+        Filter Freq
+      </p>
+      <p id="kTML2" className="subLabelXSmall">
+        LFO 2 to
+      </p>
+      <p id="kTLL4" className="subLabelXSmall">
+        Pitch
+      </p>
+      <p id="kTML3" className="subLabelXSmall">
+        Osc 2
+      </p>
+      <p id="kTLL5" className="subLabelXSmall">
+        Filter Freq
+      </p>
+      <p id="kTLL6" className="subLabelXSmall">
+        Osc Pitch
+      </p>
+      <p id="kTML4" className="subLabelXSmall">
+        LFO 1 to
+      </p>
+      <p id="kTLL7" className="subLabelXSmall">
+        speed
+      </p>
+      <p id="kTML5" className="subLabelXSmall">
+        LFO 2
+      </p>
+      <p id="kTLL8" className="subLabelXSmall">
+        LFO 1
+      </p>
+      <p id="kTML6" className="subLabelXSmall">
+        Key Sync
+      </p>
+      <p id="kTLL9" className="subLabelXSmall">
+        LFO 2
+      </p>
+      <p id="kTLL10" className="subLabelXSmall">
+        LFO 1
+      </p>
+      <p id="kTML7" className="subLabelXSmall">
+        Speed/Sync
+      </p>
+      <p id="kTLL11" className="subLabelXSmall">
+        LFO 2
+      </p>
+      <p id="kTLL12" className="subLabelXSmall">
+        LFO 1
+      </p>
+      <p id="kTML8" className="subLabelXSmall">
+        Slew
+      </p>
+      <p id="kTLL13" className="subLabelXSmall">
+        LFO 2
+      </p>
+      <p id="kTLL14" className="subLabelXSmall">
+        Range
+      </p>
+      <p id="kTML9" className="subLabelXSmall">
+        Pitch Bend
+      </p>
+      <p id="kTLL15" className="subLabelXSmall">
+        Sync
+      </p>
+      <p id="kTML10" className="subLabelXSmall">
+        Osc 1-2
+      </p>
+      <p id="kTLL16" className="subLabelXSmall">
+        Amp Env
+      </p>
+      <p id="kTLL17" className="subLabelXSmall">
+        Mod Env
+      </p>
+      <p id="kTLL18" className="subLabelXSmall">
+        Limiter
+      </p>
+      <p id="kTLL19" className="subLabelXSmall">
+        Swing
+      </p>
+      <p id="kTLL20" className="subLabelXSmall">
+        Seq Retrig
+      </p>
+      <p id="kTLL21" className="subLabelXSmall">
+        MIDI Chan
+      </p>
+      <p id="kTLL22" className="subLabelXSmall">
+        Local
+      </p>
+      <p id="kTLL23" className="subLabelXSmall">
+        Tune
+      </p>
+      <p id="kTLL24" className="subLabelXSmall">
+        Input Gain
+      </p>
+      <p id="kTLL25" className="subLabelXSmall">
+        Dump
+      </p>
     </div>
   );
 };
