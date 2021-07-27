f30af29d1b1e77e52b60c71ba8025d09f12748fc
Derek Louis <thedereklouis@gmail.com>
2021-07-27 14:40:35 -0400

    added side shadow to black keys

--- src/blackKey.js
+++ src/blackKey.js
@@ -2,15 +2,19 @@ import { useRef } from 'react';
 import './styles/blackKey.css';
 
 const BlackKey = () => {
+  const backKeyMasterInsetShadow = useRef(null);
   const blackKeyConnector = useRef(null);
   const blackKeyBottom = useRef(null);
 
   const keyDown = () => {
+    backKeyMasterInsetShadow.current.className =
+      'backKeyMasterInsetShadowActive';
     blackKeyConnector.current.className = 'blackKeyConnectorDown';
     blackKeyBottom.current.className = 'blackKeyBottomDown';
   };
 
   const keyUp = () => {
+    backKeyMasterInsetShadow.current.className = 'backKeyMasterInsetShadow';
     blackKeyConnector.current.className = 'blackKeyConnectorUp';
     blackKeyBottom.current.className = 'blackKeyBottomUp';
   };
@@ -25,6 +29,10 @@ const BlackKey = () => {
           ref={blackKeyBottom}
         />
       </div>
+      <div
+        className="backKeyMasterInsetShadow"
+        ref={backKeyMasterInsetShadow}
+      />
       <div className="blackKeyConnectorUp" ref={blackKeyConnector} />
     </div>
   );
